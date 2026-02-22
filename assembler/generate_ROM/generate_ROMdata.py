a63179e0391397de459f13d767539a27c2186b8f
Jetorific <jtrom@illinois.edu>
2026-02-22 17:11:46 -0600

    Generated schematics now include the ROM itself!

--- assembler/generate_ROM/generate_ROMdata.py
+++ assembler/generate_ROM/generate_ROMdata.py
@@ -12,7 +12,7 @@ def generate_regions(data):
     nregions = []
     sregions = []
     for i in range(8):
-        (bx, by, bz) = (34+i%2, 17, 2+7*i)
+        (bx, by, bz) = (33+i%2, 17, 1+7*i)
         nregions.append(Region(bx, by, bz, -31, 16, 1))
         sregions.append(Region(bx, by, bz+2, -31, 16, 1))
 
@@ -46,16 +46,23 @@ def main():
         return 1
     file_name = path.splitext(path.basename(file_path))[0]
     
-    print(file_path, file_name)
-    
     data = parse_bin(file_path)
     print(data.hex(sep="\t", bytes_per_sep=1))
     
     all_regions = generate_regions(data)
-    schem = Schematic(name=f"{file_name}_ROM", author="AMcD", description="ROM data generated from binary file", regions=all_regions)
+    
+    ROM_SCHEMATIC = "256_byte_ROM.litematic"
+    schem = Schematic.load(ROM_SCHEMATIC)
+    schem.name = f"{file_name}_ROMdata"
+    schem.author = schem.author + ", AMcD"
+    schem.description = "ROM data generated from binary file"
+    for name,region in all_regions.items():
+        schem.regions[name] = region
+    #schem = Schematic(name=f"{file_name}_ROM", author="AMcD", description="ROM data generated from binary file", regions=all_regions)
     
     # Save the schematic
     schem.save(f"{path.splitext(file_path)[0]}_ROMdata.litematic")
+    return 0
 
 if __name__ == "__main__":
     main()
